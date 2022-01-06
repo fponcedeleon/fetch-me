3972c2bd7b448849e6575d338d2f8b76c63ea3c6
fponcedeleon <fm.poncedeleon@gmail.com>
2022-01-06 16:48:51 -0300

    tests: update test to create a post request

--- index.test.js
+++ index.test.js
@@ -7,8 +7,12 @@ const URL = `http://127.0.0.1:${createdServer.address().port}`;
 afterAll(() => createdServer.close());
 
 describe("ensure server response", () => {
-  it("should fail with custom HTTP error", async () => {
-    const resp = await fetch(`${URL}/fetch-me`);
+  it("should return custom HTTP status code", async () => {
+    const resp = await fetch(`${URL}/fetch-me`, {
+      method: "POST",
+      body: JSON.stringify({}),
+      headers: { "Content-Type": "application/json" },
+    });
     expect(resp.status).toBe(499);
   });
 });
